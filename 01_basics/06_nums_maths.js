326c8e634cd227c9a090e4db5bf40234aa9086d2
Abhay dhiman <abhaydhiman02005@gmail.com>
2025-12-27 22:50:45 +0000

    maths and nums

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,35 @@
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966;
+
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+
+console.log(Math.max(2,54,3,5,3));
+console.log(Math.min(2,54,3,5,3));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+let min = 10;
+let max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
