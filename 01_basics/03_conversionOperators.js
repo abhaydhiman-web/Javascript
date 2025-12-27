79466a92e39967267c3a552bc6f337576fc14939
Abhay dhiman <abhaydhiman02005@gmail.com>
2025-12-27 14:07:31 +0000

    Conversion of data typed

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,29 @@
+let score = "33"
+let s = null
+
+console.log(typeof(score));
+console.log(s);
+
+
+let valueInNumber = Number(score);
+
+console.log(typeof(valueInNumber));
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false = 0
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+// 1 => true
+// " " => false
+// "yuvi" => true
+
+let aNumber = 33
+let stringNum = String(aNumber);
+
+console.log(stringNum)
+console.log(typeof stringNum);
